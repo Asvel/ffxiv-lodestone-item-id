cdc951c1eb9f506d35e5c6ec506632b84b0bbba4
Asvel <fe.asvel@gmail.com>
2025-09-11 03:16:07 +0800

    7.31 update.

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -12,7 +12,7 @@ const fetch = require('node-fetch');
 const pipeline = util.promisify(stream.pipeline);
 
 const baseUrl = 'https://na.finalfantasyxiv.com/lodestone/playguide/db/item/?page=';
-const pageCount = 859;
+const pageCount = 860;
 
 (async () => {
   fs.mkdirSync('./pages', { recursive: true });
